2f14975bb41c0b61aed9c980e250177d2116a85c
Akeeb1 <akeeebmalligawad605@gmail.com>
2024-05-18 15:40:36 +0530

    Vehicle solution

--- src/main/java/com/driver/Car.java
+++ src/main/java/com/driver/Car.java
@@ -2,10 +2,13 @@ package com.driver;
 
 public class Car extends Vehicle {
     private String model;
+    private int currentSpeed;
+
 
     public Car(String type, String model) {
         super(type);
         this.model = model;
+        this.currentSpeed = 0;
     }
 
     public String getModel() {
@@ -14,5 +17,13 @@ public class Car extends Vehicle {
 
     public void setModel(String model) {
         this.model = model;
+    }
+    public int getCurrentSpeed()
+    {
+        return currentSpeed;
+    }
+    public void setCurrentSpeed(int currentSpeed)
+    {
+        this.currentSpeed = currentSpeed;
     }
 }
