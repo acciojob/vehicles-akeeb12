d742831c9f4cb3891a9721e14148323eccd90a85
Akeeb1 <akeeebmalligawad605@gmail.com>
2024-05-18 16:16:49 +0530

    solution

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -8,14 +8,18 @@ public class Main {
 
         // Create an instance of Car
         Car car = new Car("Sedan", "Toyota Corolla");
+        car.setCurrentSpeed(100); // Set the current speed
         System.out.println("Car Type: " + car.getType());
         System.out.println("Car Model: " + car.getModel());
+        System.out.println("Car Current Speed: " + car.getCurrentSpeed() + " km/h");
 
         // Create an instance of F1
         F1 f1Car = new F1("Race Car", "Ferrari F1", 350);
+        f1Car.setCurrentSpeed(200); // Set the current speed
         System.out.println("F1 Car Type: " + f1Car.getType());
         System.out.println("F1 Car Model: " + f1Car.getModel());
         System.out.println("F1 Car Top Speed: " + f1Car.getTopSpeed() + " km/h");
+        System.out.println("F1 Car Current Speed: " + f1Car.getCurrentSpeed() + " km/h");
 
         // Create an instance of Boat
         Boat boat = new Boat("Yacht", 20);
