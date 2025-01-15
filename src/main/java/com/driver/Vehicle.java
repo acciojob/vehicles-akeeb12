fa66f7137cdf8863f3396fcd4dcc64ff6f3ac05f
akeeb12 <akeebmm@gmail.com>
2025-01-16 00:32:41 +0530

    Updated

--- src/main/java/com/driver/Vehicle.java
+++ src/main/java/com/driver/Vehicle.java
@@ -1,12 +1,12 @@
 package com.driver;
 
 public class Vehicle {
-    // private String type;
+
     private String name;
     private int currentSpeed;
     private int currentDirection;
 
-    public String getName(){
+    public String getName() {
         return name;
     }
 
@@ -15,6 +15,7 @@ public class Vehicle {
         this.currentSpeed = 0;
         this.currentDirection = 0;
     }
+
     public int getCurrentSpeed() {
         return currentSpeed;
     }
@@ -23,19 +24,22 @@ public class Vehicle {
         return currentDirection;
     }
 
-    public void move(int speed, int direction) {
-        currentSpeed = speed;
-        currentDirection = direction;
-        System.out.println("move method called - The speed is changed to: " + currentSpeed + ", and the direction is changed to: " + currentDirection + " degrees");
-    }
-
-    public void steer(int direction) {
-       currentDirection = currentDirection + direction;
+    public void steer(int direction){
+        currentDirection = currentDirection+direction;
+        //direction is in degrees, add it to the current direction
         System.out.println("steer method called - The direction is changed to: " + currentDirection + " degrees");
     }
 
-    public void stop() {
-        currentSpeed = 0;
-        System.out.println("Stop method called - The vehicle is stopped");
+    public void move(int speed, int direction){
+        currentSpeed = speed;
+        currentDirection = direction;
+        //set the values of currentSpeed and currentDirection
+        System.out.println("move method called - The speed is changed to: " + currentSpeed + ", and the direction is changed to: " + currentDirection + " degrees");
     }
+
+    public void stop(){
+       currentSpeed = 0;
+        System.out.println("stop method called - The vehicle is stopped");
+    }
+
 }
